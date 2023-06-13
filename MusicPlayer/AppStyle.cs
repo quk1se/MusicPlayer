c2166d17991fb2e233a8b2f927423e8f39514792
alexandr <KalX_si54@student.itstep.org>
2023-06-13 11:18:52 +0300

    Добавьте файлы проекта.

---
+++ MusicPlayer/AppStyle.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows;
+using System.IO.Packaging;
+
+namespace MusicPlayer
+{
+    internal class AppStyle
+    {
+        public MainWindow parent;
+        ImageBrush background = new ImageBrush(new BitmapImage(new Uri("background.png", UriKind.Relative)));
+        
+
+        private int playBtnPosition = 0;
+        private int musicIndex = 0;
+        private int repeatOnePosition = 0;
+        private int shufflePosition = 0;
+        public string[] musicFiles = Directory.GetFiles(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\music");
+
+        public int PlayBtnPosition
+        {
+            set { playBtnPosition = value; }
+            get { return playBtnPosition; }
+        }
+        public string[] MusicFiles
+        {
+            get { return musicFiles; }
+            set { musicFiles = value; }
+        }
+        public int MusicIndex
+        {
+            get { return musicIndex; }
+            set { musicIndex = value; }
+        }
+        public int RepeatOnePosition
+        {
+            set { repeatOnePosition = value; }
+            get { return repeatOnePosition; }
+        }
+        public int ShufflePosition
+        {
+            set { shufflePosition = value; }
+            get { return shufflePosition; }
+        }
+
+        public AppStyle(MainWindow parent)
+        {
+            this.parent = parent;
+        }
+        public void SetBackground()
+        {
+            background.Stretch = Stretch.UniformToFill;
+            parent.Background = background;
+        }
+        public void SetPlayButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\playBtn.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.PlayBtn.Content = image;
+            PlayBtnPosition = 1;
+        }
+        public void SetPauseButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\pauseBtn.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.PlayBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetBackButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\back.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.BackBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetNextButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\next.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.NextBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetRepeatButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\repeat.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.RepeatBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetRepeatOneButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\repeatOne.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.RepeatBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetShuffleButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\shuffle.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.RandomBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void SetNotShuffleButtonImg()
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\shuffleNon.png"));
+            RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.HighQuality);
+            RenderOptions.SetEdgeMode(image, EdgeMode.Aliased);
+            parent.RandomBtn.Content = image;
+            PlayBtnPosition = 0;
+        }
+        public void ChangeMusicName()
+        {
+            parent.MusicName.Text = Path.GetFileNameWithoutExtension(musicFiles[musicIndex]);
+        }
+    }
+}
---
+++ MusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlayer
+{
+    public partial class MainWindow : Window
+    {
+        private AppStyle appStyle;
+        private MediaPlayer mediaPlayer;
+        private bool isPaused;
+        private TimeSpan pausePosition;
+        private Slider progressSlider;
+        private DateTime lastClickTime = DateTime.MinValue;
+        public Random randomTrack = new Random();
+        public int tempPosition = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartSett();
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.Volume = 0.1;
+            mediaPlayer.MediaEnded += MediaPlayer_MediaEnded;
+        }
+
+        public void StartSett()
+        {
+            appStyle = new AppStyle(this);
+            appStyle.SetBackground();
+            appStyle.SetPlayButtonImg();
+            appStyle.SetBackButtonImg();
+            appStyle.SetNextButtonImg();
+            appStyle.ChangeMusicName();
+            appStyle.SetRepeatButtonImg();
+            appStyle.SetNotShuffleButtonImg();
+            PlayBtn.FocusVisualStyle = null;
+            this.ResizeMode = ResizeMode.NoResize;
+            this.Title = "Moseeefy";
+            this.Icon = new BitmapImage(new Uri(@"D:\\itstep\\winforms\\MusicPLayer\\MusicPLayer\\btnsImg\\moseeefy.ico", UriKind.RelativeOrAbsolute));
+        }
+
+        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (appStyle.PlayBtnPosition == 1)
+            {
+                appStyle.SetPauseButtonImg();
+                mediaPlayer.Open(new Uri(appStyle.musicFiles[appStyle.MusicIndex]));
+                
+                if (isPaused)
+                {
+                    mediaPlayer.Position = pausePosition;
+                    mediaPlayer.Play();
+                    isPaused = false;
+                }
+                else
+                {
+                    mediaPlayer.Play();
+                }
+            }
+            else if (appStyle.PlayBtnPosition == 0)
+            {
+                appStyle.SetPlayButtonImg();
+                if (mediaPlayer.CanPause)
+                {
+                    mediaPlayer.Pause();
+                    pausePosition = mediaPlayer.Position;
+                    isPaused = true;
+                }
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (mediaPlayer != null)
+            {
+                mediaPlayer.Position = TimeSpan.FromSeconds(e.NewValue);
+            }
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            tempPosition = appStyle.MusicIndex;
+            if (appStyle.PlayBtnPosition == 1) appStyle.SetPauseButtonImg();
+            if (appStyle.ShufflePosition == 1)
+            {
+                appStyle.MusicIndex = randomTrack.Next(0, appStyle.MusicFiles.Length);
+                while (tempPosition == appStyle.MusicIndex) 
+                    appStyle.MusicIndex = randomTrack.Next(0, appStyle.MusicFiles.Length);
+                appStyle.ChangeMusicName();
+            }
+            else
+            {
+                appStyle.MusicIndex += 1;
+                if (appStyle.MusicIndex == appStyle.MusicFiles.Length) appStyle.MusicIndex = 0;
+                appStyle.ChangeMusicName();
+            }
+            mediaPlayer.Open(new Uri(appStyle.musicFiles[appStyle.MusicIndex]));
+            mediaPlayer.Play();
+
+        }
+
+
+        private void BackBtn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((DateTime.Now - lastClickTime).TotalMilliseconds < 300)
+            {
+                if (appStyle.PlayBtnPosition == 1) appStyle.SetPauseButtonImg();
+                if (appStyle.MusicIndex == 0) appStyle.MusicIndex = appStyle.MusicFiles.Length;
+                appStyle.MusicIndex -= 1;
+                appStyle.ChangeMusicName();
+                mediaPlayer.Open(new Uri(appStyle.musicFiles[appStyle.MusicIndex]));
+                mediaPlayer.Play();
+            }
+            else
+            {
+                mediaPlayer.Position = TimeSpan.Zero;
+            }
+            lastClickTime = DateTime.Now;
+        }
+        private void MediaPlayer_MediaEnded(object sender, EventArgs e)
+        {
+            tempPosition = appStyle.MusicIndex;
+            if (appStyle.RepeatOnePosition == 0 && appStyle.ShufflePosition == 1)
+            {
+                appStyle.MusicIndex = randomTrack.Next(0,appStyle.MusicFiles.Length);
+                while (tempPosition == appStyle.MusicIndex)
+                    appStyle.MusicIndex = randomTrack.Next(0, appStyle.MusicFiles.Length);
+                appStyle.ChangeMusicName();
+            }
+            else if (appStyle.RepeatOnePosition == 0 && appStyle.ShufflePosition == 0)
+            {
+                appStyle.MusicIndex += 1;
+                if (appStyle.MusicIndex == appStyle.MusicFiles.Length) appStyle.MusicIndex = 0;
+                appStyle.ChangeMusicName();
+            }
+            mediaPlayer.Open(new Uri(appStyle.musicFiles[appStyle.MusicIndex]));
+            mediaPlayer.Play();
+        }
+
+        private void RepeatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (appStyle.RepeatOnePosition == 0)
+            {
+                appStyle.RepeatOnePosition = 1;
+                appStyle.SetRepeatOneButtonImg();
+            }
+            else
+            {
+                appStyle.RepeatOnePosition = 0;
+                appStyle.SetRepeatButtonImg();
+            }
+        }
+
+        private void RandomBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (appStyle.ShufflePosition == 0)
+            {
+                appStyle.ShufflePosition = 1;
+                appStyle.SetShuffleButtonImg();
+            }
+            else
+            {
+                appStyle.ShufflePosition = 0;
+                appStyle.SetNotShuffleButtonImg();
+            }
+        }
+    }
+}
